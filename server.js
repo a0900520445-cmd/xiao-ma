76e5e1d72cd26255e93e5d9703896c1a734a60de
a0900520445-cmd <a0900520445@gmail.com>
2026-05-27 21:30:58 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,35 +1,29 @@
 const express = require("express");
 const http = require("http");
+const path = require("path");
 const { WebSocketServer } = require("ws");
 
 const app = express();
 
+// ⭐ 重點：指向 public 資料夾
 app.use(express.static(path.join(__dirname, "public")));
 
 const server = http.createServer(app);
 
 const wss = new WebSocketServer({ server });
 
-wss.on("connection",(ws)=>{
+wss.on("connection", (ws) => {
+    console.log("玩家連線");
 
-    console.log("玩家加入");
-
-    ws.on("message",(msg)=>{
-
-        console.log(msg.toString());
-
-        wss.clients.forEach(client=>{
-
-            if(client.readyState === 1){
+    ws.on("message", (msg) => {
+        wss.clients.forEach(client => {
+            if (client.readyState === 1) {
                 client.send(msg.toString());
             }
-
         });
-
     });
-
 });
 
-server.listen(3000,()=>{
-    console.log("Server running: http://localhost:3000");
+server.listen(3000, () => {
+    console.log("http://localhost:3000");
 });
