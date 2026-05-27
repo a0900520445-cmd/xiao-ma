9baaf4b468efa8f1bc66f24a5cb6f9eab9585359
a0900520445-cmd <a0900520445@gmail.com>
2026-05-27 21:28:12 +0800

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const { WebSocketServer } = require("ws");
 
 const app = express();
 
-app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, "public")));
 
 const server = http.createServer(app);
 
