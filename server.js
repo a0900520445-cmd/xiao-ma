80d0cf2b9ed138f1c4f065c92336c8637334350f
a0900520445-cmd <a0900520445@gmail.com>
2026-06-10 17:39:50 +0800

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,11 @@ import session from 'express-session'
 import bodyParser from 'body-parser'
 import path from 'path'
 import pg from 'pg'
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const { Pool } = pg
 
